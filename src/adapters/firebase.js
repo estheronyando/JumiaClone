deb9058a23dbc09edeb5ed1784877bc3a690908c
Esther <eonyando89@gmail.com>
2023-08-07 11:02:12 +0300

    Adding Jumia Frontend Clone files

--- src/adapters/firebase.js
+++ src/adapters/firebase.js
@@ -4,22 +4,16 @@ import { initializeApp } from "firebase/app";
 
 
 
-// apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_ID,
-  // measurementId: process.env.MEASUREMENT_ID
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDd2UDG8VoBWJBTfIMy71YBdrW2Fy-iJBY",
-  authDomain: "jumia-871c3.firebaseapp.com",
-  projectId: "jumia-871c3",
-  storageBucket: "jumia-871c3.appspot.com",
-  messagingSenderId: "130282456974",
-  appId: "1:130282456974:web:8877305f1661f849f746df",
-  measurementId: "G-ZW2LVV9HRV"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
   
   
 };
